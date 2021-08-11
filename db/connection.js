8d723e2f7a895cf8dbb89c2dcdf68a85dae5da53
Olga <o.smolyakova30@gmail.com>
2021-08-11 17:16:43 -0500

    removed data

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '54459377Dud!',
+    password: '',
     database: 'election'
   });
 
